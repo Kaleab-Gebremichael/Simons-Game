3beb4f96cfbd6013d9bc5bbbddaad331479cbe76
Kaleab Gebremichael <48499851+Kaleab470@users.noreply.github.com>
2020-01-14 11:20:34 -0600

    answer-checking complete (#1)

--- game.js
+++ game.js
@@ -4,30 +4,67 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-var userClickedPattern = [];
+var started = false;
+var level = 0;
 
-//detect when any of the buttons are clicked
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
-
-  //update the userClickedPattern
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
-
   animatePress(userChosenColour);
 
+  //pass in the index of the last answer in the user's sequence.
+  checkAnswer(userClickedPattern.length-1);
 });
 
+
+function checkAnswer(currentLevel) {
+
+    //check if the most recent user answer is the same as the game pattern.
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      //additional check that they have finished their sequence
+      if (userClickedPattern.length === gamePattern.length){
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+
+    }
+
+}
+
 function nextSequence() {
+
+  //reset the userClickedPattern to an empty array ready for the next level
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
   playSound(randomChosenColour);
 }
 
@@ -37,11 +74,7 @@ function playSound(name) {
 }
 
 function animatePress(currentColor) {
-
-  //add pressed class to pressed button
   $("#" + currentColor).addClass("pressed");
-
-  //remove pressed class after a 100 milliseconds.
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
